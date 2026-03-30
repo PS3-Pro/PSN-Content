2051f2fbc07cd7cb0c62c950c68fb9662d1ee881
LuanTeles <110236244+LuanTeles@users.noreply.github.com>
2026-03-30 10:35:59 -0300

    Release

--- files/sw.js
+++ files/sw.js
@@ -1,1 +1,7 @@
+self.addEventListener('install', function(event) {
+    console.log('PWA: Service Worker installed.');
+    self.skipWaiting();
+});
+
 self.addEventListener('fetch', function(event) {
+});
