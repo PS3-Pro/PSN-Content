9fd99bd211e92fff618a23443d589cd490a5cc7a
LuanTeles <110236244+LuanTeles@users.noreply.github.com>
2026-04-05 01:06:01 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const http = require('http');
+const https = require('https');
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
+const APP_URL = "https://psn-content.onrender.com/ping"; 
+
+app.get('/ping', (req, res) => {
+  res.send('Servidor Acordado!');
+});
+
+setInterval(() => {
+  https.get(APP_URL, (res) => {
+    console.log(`Auto-ping: Status ${res.statusCode} - Mantendo o motor ligado!`);
+  }).on('error', (err) => {
+    console.error("Erro no auto-ping:", err.message);
+  });
+}, 840000);
+// ---------------------------------
 
 const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST"] }
@@ -19,7 +35,7 @@ io.on('connection', (socket) => {
   socket.on('chat_message', (msg) => {
     messageHistory.push(msg);
 
-    if (messageHistory.length > 500) {
+    if (messageHistory.length > 1000) {
       messageHistory.shift(); 
     }
 
