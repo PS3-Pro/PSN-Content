2fd1ffeedb7dca82fa9fc129eab23f7a8ea3a29b
LuanTeles <110236244+LuanTeles@users.noreply.github.com>
2026-04-05 00:05:12 -0300

    Release

--- server/server.js
+++ server/server.js
@@ -6,17 +6,24 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*", 
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: "*", methods: ["GET", "POST"] }
 });
+
+let messageHistory = []; 
 
 io.on('connection', (socket) => {
   console.log('User connected: ' + socket.id);
 
+  socket.emit('chat_history', messageHistory);
+
   socket.on('chat_message', (msg) => {
-    io.emit('chat_message', msg); 
+    messageHistory.push(msg);
+
+    if (messageHistory.length > 100) {
+      messageHistory.shift(); 
+    }
+
+    socket.broadcast.emit('chat_message', msg); 
   });
 
   socket.on('disconnect', () => {
