63a7422553a851a9662f1f7d3dd532e70c3484d9
LuanTeles <110236244+LuanTeles@users.noreply.github.com>
2026-04-05 01:09:58 -0300

    Release

--- server/server.js
+++ server/server.js
@@ -9,17 +9,16 @@ const server = http.createServer(app);
 const APP_URL = "https://psn-content.onrender.com/ping"; 
 
 app.get('/ping', (req, res) => {
-  res.send('Servidor Acordado!');
+  res.send('Server is Awake!');
 });
 
 setInterval(() => {
   https.get(APP_URL, (res) => {
-    console.log(`Auto-ping: Status ${res.statusCode} - Mantendo o motor ligado!`);
+    console.log(`Auto-ping: Status ${res.statusCode} - Keeping the engine running!`);
   }).on('error', (err) => {
-    console.error("Erro no auto-ping:", err.message);
+    console.error("Auto-ping error:", err.message);
   });
 }, 840000);
-// ---------------------------------
 
 const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST"] }
@@ -33,13 +32,18 @@ io.on('connection', (socket) => {
   socket.emit('chat_history', messageHistory);
 
   socket.on('chat_message', (msg) => {
-    messageHistory.push(msg);
+    const messageData = {
+      text: msg,
+      time: new Date().toISOString()
+    };
+
+    messageHistory.push(messageData);
 
     if (messageHistory.length > 1000) {
       messageHistory.shift(); 
     }
 
-    socket.broadcast.emit('chat_message', msg); 
+    socket.broadcast.emit('chat_message', messageData); 
   });
 
   socket.on('disconnect', () => {
